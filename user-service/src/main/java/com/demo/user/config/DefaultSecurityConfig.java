0340f0e86e9e79cc97e1b3d2cc031aeb942c5d43
kimjongil <whddlf0504@naver.com>
2025-02-20 21:57:25 +0900

    사용자 서비스 추가.

--- user-service/src/main/java/com/demo/user/config/DefaultSecurityConfig.java
+++ user-service/src/main/java/com/demo/user/config/DefaultSecurityConfig.java
@@ -1,9 +1,9 @@
 package com.demo.user.config;
 
-import com.example.common.security.SaltPasswordEncoder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Slf4j
@@ -11,6 +11,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class DefaultSecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new SaltPasswordEncoder();
+        return new BCryptPasswordEncoder();
     }
 }
