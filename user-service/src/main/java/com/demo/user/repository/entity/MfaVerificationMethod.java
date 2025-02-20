a99d49f3b4018247a48fad43a3e2aeb71df535bd
kimjongil <whddlf0504@naver.com>
2025-02-20 23:34:51 +0900

    사용자 엔티티 필드 추가.

---
+++ user-service/src/main/java/com/demo/user/repository/entity/MfaVerificationMethod.java
@@ -0,0 +1,14 @@
+package com.demo.user.repository.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+public enum MfaVerificationMethod {
+    NONE("NONE"),
+    EMAIL("EMAIL"),
+    PHONE("PHONE");
+    private final String value;
+}
